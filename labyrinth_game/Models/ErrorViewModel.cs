96b0b503b5d6e643fe18bd17ec3b92b5f0e2cb17
Katri Lotamõis <katrilotamois@outlook.com>
2026-09-01 10:06:12 +0300

    create project structure

---
+++ labyrinth_game/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace labyrinth_game.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
